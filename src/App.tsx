20336e3cd40e229bf781666e1ce762a9f637288c
Patrik Relovsky <pato.relovsky@gmail.com>
2024-02-13 14:43:37 +0100

    Add post form component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 
 export default function App() {
   return (
     <div className="container m-1">
+      <PostForm />
       <PostList />
     </div>
   );
