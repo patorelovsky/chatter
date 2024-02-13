300a62cd9f3ed5373088481f66052f58879292a9
Patrik Relovsky <pato.relovsky@gmail.com>
2024-02-13 14:14:09 +0100

    Style the heading

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -2,12 +2,12 @@ import { useFetchPostsQuery } from "../redux";
 import Post from "./Post";
 
 export default function PostList() {
-  const { error, isFetching, data } = useFetchPostsQuery(1);
+  const { isError, isFetching, data } = useFetchPostsQuery(1);
 
   const getContent = () => {
     if (isFetching) {
       return <div>Loading...</div>;
-    } else if (error) {
+    } else if (isError) {
       return <div>Error loading posts.</div>;
     } else {
       return data?.map((post) => <Post key={post.id} post={post} />);
@@ -16,7 +16,7 @@ export default function PostList() {
 
   return (
     <div>
-      <h1>Posts</h1>
+      <h1 className="font-medium text-xl">Posts</h1>
       <div>{getContent()}</div>
     </div>
   );
