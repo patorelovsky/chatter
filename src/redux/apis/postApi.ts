333cf0659596ab20dd7e6f58b474d932a538d1c7
Patrik Relovsky <pato.relovsky@gmail.com>
2024-02-15 11:14:53 +0100

    Add per page to get post api args

--- src/redux/apis/postApi.ts
+++ src/redux/apis/postApi.ts
@@ -1,20 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { Post } from "../../types";
 
-const PER_PAGE = 10;
+type GetPostsArgs = {
+  _page: number;
+  _per_page: number;
+};
 
 export const postApi = createApi({
   reducerPath: "postApi",
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   tagTypes: ["Post"],
   endpoints: (builder) => ({
-    getPosts: builder.query<Post[], number>({
-      query: (page = 1) => ({
+    getPosts: builder.query<Post[], GetPostsArgs>({
+      query: (params) => ({
         url: `/posts`,
-        params: {
-          _page: page,
-          _per_page: PER_PAGE,
-        },
+        params,
       }),
       transformResponse: ({ data }: { data: Post[] }) => data,
       providesTags: ["Post"],
@@ -27,7 +27,7 @@ export const postApi = createApi({
       }),
       invalidatesTags: ["Post"],
     }),
-    deletePost: builder.mutation<void, Partial<Post> & Pick<Post, "id">>({
+    deletePost: builder.mutation<void, Pick<Post, "id">>({
       query: ({ id }) => ({
         url: `/posts/${id}`,
         method: "DELETE",
